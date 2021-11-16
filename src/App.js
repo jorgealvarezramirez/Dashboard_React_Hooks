04d4fdfa9f6aa5ca5d4f7f51cb1dd4bb0b054252
jorgealvarezramirez <jaar77@gmail.com>
2021-11-16 14:18:38 -0500

    Creando Rutas Dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MiPrimerComponente from "./Components/MiPrimerComponente";
-import { Calculadora } from "./Components/Calculadora";
 import Dashboard from "./Components/Dashboard";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <Dashboard />
-        <br />
-        <Calculadora />
-        <br />
-        <MiPrimerComponente />
-        <br />
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,21 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Misión Tic</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Mi Primer Componente</Nav.Link>
+            <Nav.Link href="#pricing">Calculadora</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
